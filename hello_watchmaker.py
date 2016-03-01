a9f4e20434e4bd6155319ede9c3083bd3b0f9dea
Francisco Lang <flang@renare.uchile.cl>
2016-03-01 16:00:11 +1100

    comentarios comandos

--- hello_watchmaker.py
+++ hello_watchmaker.py
@@ -1,2 +1,8 @@
 print("Greetings! How can I help you\n?")
 print("Greetings! How can I help you2?")
+
+cambiar
+git remote set-url origin https://github.com/user/repo2.git
+
+fuerza a reescribir
+push -f
